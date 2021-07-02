6425a55bab9f78e938f5c6da280052dbaa8825a8
unknown <rahzy24@gmail.com>
2021-07-02 09:40:08 +0100

    ui build

---
+++ pages/ProductDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="product in products" :key="product.id">
+              <h1>{{ product.name }}</h1>
+              <h1>{{ product.text }}</h1>
+              <h1>{{ product.features }}</h1>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"ProductDetails",
+    data() {
+        return {
+            products: [
+                {name:'yx1', text:"Best in earphones lormem", features: 'ytmkfs'},
+                {name:'yx1', text:"Best in earphones lormem", features: 'ytmkfs'},
+                {name:'yx1', text:"Best in earphones lormem", features: 'ytmkfs'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
